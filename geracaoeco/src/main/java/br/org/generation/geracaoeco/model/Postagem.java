4ea9741ff67adb8690dc45d7e8e3eb6c91419f7e
Your Name <you@example.com>
2022-01-27 10:56:44 -0300

    Incluindo Repository e Controller de postagem

--- geracaoeco/src/main/java/br/org/generation/geracaoeco/model/Postagem.java
+++ geracaoeco/src/main/java/br/org/generation/geracaoeco/model/Postagem.java
@@ -6,13 +6,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
-@Table(name = "Tb_postagem")
+@Table(name = "tb_postagem")
 public class Postagem {
 	
 	@Id
@@ -32,6 +35,9 @@ public class Postagem {
 	
 	@NotBlank(message = "O campo Região é obrigatório!")
 	private String regiao;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
 
 	public Long getId() {
 		return id;
